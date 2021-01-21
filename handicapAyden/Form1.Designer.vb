b47ad833acc1630552c765002c452d2be7b5e6fa
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-21 14:20:07 -0600

    Initial commit

---
+++ handicapAyden/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnScore = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnScore
+        '
+        Me.btnScore.Location = New System.Drawing.Point(103, 119)
+        Me.btnScore.Name = "btnScore"
+        Me.btnScore.Size = New System.Drawing.Size(79, 23)
+        Me.btnScore.TabIndex = 0
+        Me.btnScore.Text = "Enter Scores"
+        Me.btnScore.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnScore)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnScore As System.Windows.Forms.Button
+
+End Class
---
+++ handicapAyden/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub btnScore_Click(sender As Object, e As EventArgs) Handles btnScore.Click
+        Dim intCount As Integer
+        Dim intNum As Integer
+        Dim sngDiff As Single
+        Dim intAverage As Integer
+        Const COURSE As Integer = 69
+        Const SLOPE As Integer = 94
+
+        For intCount = 1 To 5
+            intNum = Val(InputBox("Enter Score" & intCount))
+            sngDiff = sngDiff + (intNum - COURSE) * 113 / SLOPE
+        Next
+
+        intAverage = (sngDiff) \ 5
+        MessageBox.Show(intAverage)
+
+    End Sub
+End Class
